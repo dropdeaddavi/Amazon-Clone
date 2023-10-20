15a56551ee61e181c11f710b257c3ca831495ee8
dropdeaddavi <civimonet@gmail.com>
2023-10-20 17:52:24 -0500

    moved addToCart function, imported to amazon.js

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,22 @@
 export const cart = [
     
 ];
+
+export function addToCart(productId, selectorValue){
+    let matchingItem;
+
+    cart.forEach((item) =>{
+        if (productId === item.productId) {
+            matchingItem = item;
+        }
+    });
+
+    if (matchingItem) {
+        matchingItem.quantity += selectorValue;
+    } else{
+        cart.push({
+            productId: productId,
+            quantity: selectorValue
+        });
+    }
+}
