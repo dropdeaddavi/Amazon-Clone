3daf06903a863e875af3d00ccc8faf3134851919
dropdeaddavi <civimonet@gmail.com>
2023-10-20 15:00:00 -0500

    Adjusted quantity selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=>{
         </div>
 
         <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -53,11 +53,18 @@ products.forEach((product)=>{
   </div>`;
 })
 
+
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => { 
+
         const productId = button.dataset.productId;
+
+        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
+        const selectorValue = Number(quantitySelector.value);
+
 
         let matchingItem;
 
@@ -68,11 +75,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         });
 
         if (matchingItem) {
-            matchingItem.quantity += 1;
+            matchingItem.quantity += selectorValue;
         } else{
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: selectorValue
             });
         }
 
