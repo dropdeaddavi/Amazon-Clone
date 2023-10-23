1582cca874ef197367d80970305ceb98112932c8
dropdeaddavi <civimonet@gmail.com>
2023-10-23 13:24:20 -0500

    delete products from HTML when clicking delete

--- data/checkout.js
+++ data/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "./cart.js";
+import { cart, removeFromCart } from "./cart.js";
 import { products } from "./products.js";
 import { formatCurrency } from "../scripts/utils/money.js";
 
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
 
     cartSummaryHTML +=
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -40,7 +40,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -93,6 +93,15 @@ cart.forEach((cartItem) => {
         </div>
     </div>
     `;
-
-    document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 });
+
+document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click', () => {
+       const productId =  link.dataset.productId;
+       removeFromCart(productId);
+       const container = document.querySelector(`.js-cart-item-container-${productId}`);
+       container.remove();
+    });
+})
